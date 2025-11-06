eb0e8c6ad4f860e725866cef474d13459043a9c0
goqiewd <cheari1018@gmail.com>
2025-11-06 16:55:29 +0900

    init commit

--- zombie.py
+++ zombie.py
@@ -56,9 +56,6 @@ class Zombie:
             Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, 200, 200)
         draw_rectangle(*self.get_bb())
 
-    def get_bb(self):
-        return self.x - 60, self.y - 90, self.x + 60, self.y + 90
-
     def handle_event(self, event):
         pass
 
